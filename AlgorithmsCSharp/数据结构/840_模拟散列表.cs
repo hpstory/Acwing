54ce7bab258ebfda3129a31054ac6001c90a8c3c
hpstory <33348162+hpstory@users.noreply.github.com>
2022-05-18 01:19:58 +0800

    update

---
+++ AlgorithmsCSharp/数据结构/840_模拟散列表.cs
@@ -0,0 +1,45 @@
+﻿namespace _840_模拟散列表
+{
+    class Program
+    {
+        static int[] hash;
+
+        const int none = 0x3f3f3f3f;
+
+        const int N = 200003;
+
+        static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            hash = new int[N];
+            Array.Fill(hash, none);
+            while (n-- > 0)
+            {
+                string op = Console.ReadLine();
+                int x = Convert.ToInt32(Console.ReadLine());
+                int k = Find(x);
+                if (op == "I")
+                {
+                    hash[k] = x;
+                }
+                else if (op == "Q")
+                {
+                    if (hash[k] != none) Console.WriteLine("Yes");
+                    else Console.WriteLine("No");
+                }
+            }
+        }
+
+        static int Find(int x)
+        {
+            int k = (x % N + N) % N;
+            while (hash[k] != none && hash[k] != x)
+            {
+                k++;
+                if (k == N) k = 0;
+            }
+
+            return k;
+        }
+    }
+}
---
+++ AlgorithmsCSharp/数据结构/841_字符串哈希.cs
@@ -0,0 +1,43 @@
+﻿namespace _841_字符串哈希
+{
+    class Program
+    {
+        static ulong[] hash;
+
+        static ulong[] pow;
+
+        static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            string s = " " + Console.ReadLine();
+            int N = 100010;
+            int P = 131;
+            hash = new ulong[N];
+            pow = new ulong[N];
+
+            pow[0] = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                pow[i] = pow[i - 1] * (ulong)P;
+                hash[i] = hash[i - 1] * (ulong)P + s[i];
+            }
+
+            while (m-- > 0)
+            {
+                int l1 = Convert.ToInt32(Console.ReadLine());
+                int r1 = Convert.ToInt32(Console.ReadLine());
+                int l2 = Convert.ToInt32(Console.ReadLine());
+                int r2 = Convert.ToInt32(Console.ReadLine());
+
+                if (Find(l1, r1) == Find(l2, r2)) Console.WriteLine("Yes");
+                else Console.WriteLine("No");
+            }
+        }
+
+        static ulong Find(int l, int r)
+        {
+            return hash[r] - hash[l - 1] * pow[r - l + 1];
+        }
+    }
+}
